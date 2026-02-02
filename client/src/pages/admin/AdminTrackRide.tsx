6569b082d48b2ff8eabc0f92dfd1257d6255b407
AbhiGandhi02 <abhigandhi0212@gmail.com>
2026-02-03 00:27:17 +0530

    Resolved build error

--- client/src/pages/admin/AdminTrackRide.tsx
+++ client/src/pages/admin/AdminTrackRide.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate, useSearchParams } from 'react-router-dom';
 import { ScheduledRide, RideStatusUpdate, RideLocationUpdate } from '../../types';
 import { scheduledRideAPI } from '../../services/api';
